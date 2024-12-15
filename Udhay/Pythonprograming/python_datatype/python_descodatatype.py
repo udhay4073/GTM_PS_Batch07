9846cc6da2acf94270118e83207707193a375803
python3 <udhaychowdary939@gmail.com>
2024-12-15 12:36:49 +0530

    Adding fill to for python varibles

---
+++ Udhay/Pythonprograming/python_datatype/python_descodatatype.py
@@ -0,0 +1,18 @@
+python data type
+1. number
+    i) integer  ii) float iii) complex numberb
+
+2. sequential
+       i) string ii) list iii) tuple
+
+3 dictionary
+4. set
+5. boolean
+
+############  integer data type ##########
+var = 100
+print(var1)
+print(type(var1))
+"""
+
+ integer is immutable data type 
